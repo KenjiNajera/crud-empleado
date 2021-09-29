2ccc86f5ce9c79016a27a65a9700a2498fe1d0a3
Kenji <kenji.naj@outlook.com>
2021-09-29 10:43:56 -0500

    nuevas tablas, correccion crear, boostrap 4

--- index.js
+++ index.js
@@ -14,103 +14,109 @@ app.use(express.urlencoded({ extended: false }));
 //base de datos postgres
 // Connection to the PostgreSQL database
 const pool = new Pool({
-    user: "pikpgpor",
-    host: "fanny.db.elephantsql.com",
-    database: "pikpgpor",
-    password: "S3T2Ns4EVpNcSsWOaTaPT0LXAwODC4Yr",
-    port: 5432
-  });
-  console.log("Successful connection to the database");
+  user: "pikpgpor",
+  host: "fanny.db.elephantsql.com",
+  database: "pikpgpor",
+  password: "S3T2Ns4EVpNcSsWOaTaPT0LXAwODC4Yr",
+  port: 5432,
+});
+console.log("Successful connection to the database");
 
+// Starting the server
+app.listen(port, () => {
+  console.log("Server started (http://localhost:$port/) !");
+});
 
+// GET /
 
-  
-  // Starting the server
-  app.listen(port, () => {
-    console.log("Server started (http://localhost:$port/) !");
+// GET /index
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM empleado";
+
+  pool.query(sql, [], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("index", { model: result.rows });
   });
-  
-  // GET /
-  
-  
-  
-  
-  // GET /index
-  app.get("/", (req, res) => {
-    const sql = "SELECT * FROM empleado";
-    
-    pool.query(sql, [], (err, result) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      console.log(result.rows);
-      res.render("index", { model: result.rows });
-    });
+});
+
+// GET /create
+app.get("/create", (req, res) => {
+  res.render("create", { model: {} });
+});
+
+// POST /create
+app.post("/create", (req, res) => {
+  const sql =
+    "INSERT INTO empleado (nombre, apellido, correo, fechaIngreso) VALUES ($1, $2, $3, $4)";
+  const empl = [
+    req.body.nombre,
+    req.body.apellido,
+    req.body.correo,
+    req.body.fechaIngreso,
+  ];
+
+  pool.query(sql, empl, (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/");
   });
-  
-  // GET /create
-  app.get("/create", (req, res) => {
-    res.render("create", { model: {} });
+});
+
+// GET /edit/5
+app.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM empleado WHERE id = $1";
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("edit", { model: result.rows[0] });
   });
-  
-  // POST /create
-  app.post("/create", (req, res) => {
-    const sql = "INSERT INTO empleado (nombre, correo, fechaIngreso) VALUES ($1, $2, $3)";
-    const book = [req.body.nombre, req.body.correo, req.body.fechaIngreso];
-    
-    pool.query(sql, book, (err, result) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      res.redirect("/");
-    });
+});
+
+// POST /edit/5
+app.post("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const empl = [
+    req.body.nombre,
+    req.body.apellido,
+    req.body.correo,
+    req.body.fechaIngreso,
+    id,
+  ];
+  const sql =
+    "UPDATE empleado SET nombre = $1, apellido=$2, correo = $3, fechaIngreso = $4 WHERE (id = $5)";
+  pool.query(sql, empl, (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/");
   });
-  
-  // GET /edit/5
-  app.get("/edit/:id", (req, res) => {
-    const id = req.params.id;
-    const sql = "SELECT * FROM empleado WHERE id = $1";
-    pool.query(sql, [id], (err, result) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      res.render("edit", { model: result.rows[0] });
-    });
+});
+
+// GET /delete/5
+app.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM empleado WHERE id = $1";
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("delete", { model: result.rows[0] });
   });
-  
-  // POST /edit/5
-  app.post("/edit/:id", (req, res) => {
-    const id = req.params.id;
-    const book = [req.body.nombre, req.body.correo, req.body.fechaIngreso, id];
-    const sql = "UPDATE empleado SET nombre = $1, correo = $2, fechaIngreso = $3 WHERE (id = $4)";
-    pool.query(sql, book, (err, result) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      res.redirect("/");
-    });
+});
+
+// POST /delete/5
+app.post("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM empleado WHERE id = $1";
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/");
   });
-  
-  // GET /delete/5
-  app.get("/delete/:id", (req, res) => {
-    const id = req.params.id;
-    const sql = "SELECT * FROM empleado WHERE id = $1";
-    pool.query(sql, [id], (err, result) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      res.render("delete", { model: result.rows[0] });
-    });
-  });
-  
-  // POST /delete/5
-  app.post("/delete/:id", (req, res) => {
-    const id = req.params.id;
-    const sql = "DELETE FROM empleado WHERE id = $1";
-    pool.query(sql, [id], (err, result) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      res.redirect("/");
-    });
-  });
+});
