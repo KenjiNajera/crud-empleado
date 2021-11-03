05a77028d5d3ae662fb6046e4d35b5842943a50a
Kenji <kenji.naj@outlook.com>
2021-11-03 11:53:11 -0600

    deploys

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Main  from "./prueba";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Main/>
+     <App /> 
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
